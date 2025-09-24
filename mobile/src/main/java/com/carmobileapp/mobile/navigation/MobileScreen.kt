3e206d91443ebe1b2736d9e86e7a18e133bc2a5a
Ricardo <ricardocaldeira@gmail.com>
2025-09-23 22:47:28 -0300

    Delete mobile directory

--- mobile/src/main/java/com/carmobileapp/mobile/navigation/MobileScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.carmobileapp.mobile.navigation
-
-sealed class MobileScreen(val route: String) {
-    object Home : MobileScreen("home")
-    object Navigation : MobileScreen("navigation")
-    object Vehicles : MobileScreen("vehicles")
-    object Profile : MobileScreen("profile")
-}
